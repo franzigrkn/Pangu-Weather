f082df61df54cb05221c5b473f872870973fa2e1
Franziska Gerken <fgerken@ipp1-1834.ipp1a1.colossus.nvidia.com>
2025-03-21 10:49:37 +0000

    Spped up dataloading for surface gt data in eval

--- pangu/evaluation/checkout_data_sample.py
+++ pangu/evaluation/checkout_data_sample.py
@@ -3,16 +3,17 @@ import numpy as np
 import os
 import time
 
-import tables
-tables.file._open_files.close_all()
-
 data_dir = '/data/pangu'
-path_vds_gt_surface = os.path.join(data_dir, 'virtual_ds/evaluation/VDS_GT_surface.h5')
-path_vds_gt_upper = os.path.join(data_dir, 'virtual_ds/evaluation/VDS_GT_atmospheric.h5')
+path_vds_gt_surface = os.path.join(data_dir, 'raw_data/surface/msl_2018_PART1.h5')
+path_vds_gt_surface_full = os.path.join(data_dir, 'raw_data/surface/msl_2018.nc')
+print(f"gt surface: {path_vds_gt_surface}")
+print(f"gt upper: {path_vds_gt_surface_full}")
 
 
 with h5py.File(path_vds_gt_surface, "r") as f_gt_surface:
     print(f"dataset gt surface: {f_gt_surface}")
+    print(f"Shape of gt surface : {f_gt_surface['msl'].shape}")
 
-with h5py.File(path_vds_gt_upper, "r") as f_gt_upper:
-    print(f"dataset gt upper: {f_gt_upper}")
+with h5py.File(path_vds_gt_surface_full, "r") as f_gt_full:
+    print(f"dataset gt upper: {f_gt_full}")
+    print(f"Shape of gt upper : {f_gt_full['msl'].shape}")
--- pangu/evaluation/evaluate_2018.py
+++ pangu/evaluation/evaluate_2018.py
@@ -7,7 +7,7 @@ import time
 from loss import WeatherBenchLoss_plain
 
 #####################
-mode = 'atmosphere' # surface or atmosphere
+mode = 'surface' # surface or atmosphere
 every_2nd = True
 #####################
 
@@ -26,22 +26,31 @@ def create_virtual_source(mode, data_dir, vars, vds_filename):
     # INPUT SURFACE
     if mode=='surface_input':
         start = time.time()
-        ex_ds_filename = os.path.join(data_dir, f"raw_data/surface/msl_2018.nc")
+        ex_ds_filename = os.path.join(data_dir, f"raw_data/surface/msl_2018_PART1.h5")
         with h5py.File(ex_ds_filename, 'r') as f:
             sh = f["msl"].shape
             print(f"Shape surface data: {sh}")
-            layout = h5py.VirtualLayout(shape=(4,) + sh, dtype=np.float32)
+            print(f"Shape for layout: {(1460,4,) + sh[1:]}")
+            layout = h5py.VirtualLayout(shape=(1460,4,) + sh[1:], dtype=np.float32)
         for i, var in enumerate(vars):
-            # entry key 
-            filename = os.path.join(data_dir, f"raw_data/surface/{var}_2018.nc")
-            if var == 'u10m':
-                entry_key = 'u10'
-            elif var == 'v10m':
-                entry_key = 'v10'
-            else:
-                entry_key = var
-            vsource = h5py.VirtualSource(filename, entry_key, shape=sh)
-            layout[i, :, :, :] = vsource
+            for part in range(1,16):
+                if part==15:
+                    start = (part-1)*100
+                    end = 1460
+                else:
+                    start = (part-1)*100
+                    end = part*100
+                # entry key 
+                filename = os.path.join(data_dir, f"raw_data/surface/{var}_2018_PART{part}.h5")
+                if var == 'u10m':
+                    entry_key = 'u10'
+                elif var == 'v10m':
+                    entry_key = 'v10'
+                else:
+                    entry_key = var
+                virtual_shape = (end-start,1) + sh[1:]
+                vsource = h5py.VirtualSource(filename, entry_key, shape=virtual_shape)
+                layout[start:end, i, :, :] = vsource
         # Add virtual dataset to output file
         vds_path = os.path.join(vds_dir, vds_filename)
         with h5py.File(vds_path, 'w', libver='latest') as f:
@@ -168,11 +177,11 @@ def evaluation(mode, every_2nd, save_dir):
 
     # Data samples
     if every_2nd:
-        eval_range = range(0, 1460, 2)
-        #eval_range = range(0, 8, 2)
+        #eval_range = range(0, 1460, 2)
+        eval_range = range(0, 10, 2)
     else:
-        eval_range = range(1460)
-        #eval_range = range(3)
+        #eval_range = range(1460)
+        eval_range = range(5)
 
     # LOSS
     loss_fn = WeatherBenchLoss_plain()
@@ -183,8 +192,6 @@ def evaluation(mode, every_2nd, save_dir):
         path_vds_gt_surface = create_virtual_source(mode='surface_input', data_dir=data_dir, vars=vars_surface, vds_filename=f"VDS_GT_surface.h5")
         path_vds_preds_surface = create_virtual_source(mode='surface_preds', data_dir=data_dir, vars=vars_surface, vds_filename=f"VDS_PREDS_surface.h5")
         print(f"*** EVALUATING SURFACE VARIABLES ***")
-        # create np array of shape 4 x 1460
-        loss_surface = np.zeros((4, 1))
         start_total = time.time()
         with h5py.File(path_vds_gt_surface, "r") as f_gt_surface:
             with h5py.File(path_vds_preds_surface, "r") as f_preds_surface:
@@ -194,7 +201,7 @@ def evaluation(mode, every_2nd, save_dir):
                     print(f"*** ITERATION {i} ***")
                     # Loads preds and ground truth
                     start = time.time()
-                    gt = f_gt_surface['data'][:, i, :, :]
+                    gt = f_gt_surface['data'][i, :, :, :]
                     preds = f_preds_surface['data'][i-1, :, :, :]
                     print(f"    Shape of gt: {gt.shape}")
                     print(f"    Shape of preds: {preds.shape}")
@@ -203,18 +210,21 @@ def evaluation(mode, every_2nd, save_dir):
                     # Evaluate
                     start = time.time()
                     loss = loss_fn.compute_loss(gt, preds)
+                    loss = np.expand_dims(loss, 1)
                     print(f"    Shape of loss: {loss.shape}")
                     if i==0:
                         loss_surface = loss
                     else:
-                        loss_surface = np.concatenate((loss_surface, loss), axis=-1)
+                        loss_surface = np.concatenate((loss_surface, loss), axis=1)
                     print(f"    Loss surface shape: {loss_surface.shape}")
                     print(f"    Loss: {loss}")
                     end = time.time()
                     print(f"    Time taken for evaluating one datapoint: {end-start} secs.\n")
                 # Average over all time steps
+                print(f"Shape of loss_surface: {loss_surface.shape}")
                 final_loss_surface = np.mean(loss_surface, axis=-1)
                 print(f"Final shape of loss: {final_loss_surface.shape}")
+                print(f"Final loss SURFACE [msl, u10m, v10m, t2m]: {final_loss_surface}")
             end_total = time.time()
             print(f"Total time taken for evaluating all datapoints: {end_total-start_total} secs.")
             print(f"*** END OF EVALUATION SURFACE VARIABLES ***\n")
@@ -229,8 +239,6 @@ def evaluation(mode, every_2nd, save_dir):
         path_vds_preds_upper = create_virtual_source(mode='upper_preds', data_dir=data_dir, vars=vars_surface, vds_filename=f"VDS_PREDS_atmospheric.h5")
         # Evaluate SURFACE
         print(f"*** EVALUATING ATMOSPHERIC VARIABLES ***")
-        # create np array of shape 5 x 13 x 1460
-        loss_upper = np.zeros((5, 13, 1460))
         start_total = time.time()
         with h5py.File(path_vds_gt_upper, "r") as f_gt_upper:
             with h5py.File(path_vds_preds_upper, "r") as f_preds_upper:
@@ -244,18 +252,24 @@ def evaluation(mode, every_2nd, save_dir):
                     print(f"    Shape of preds: {preds.shape}")
                     end = time.time()
                     print(f"    Time taken for loading one sample: {end-start} secs.")
-
                     # Evaluate
                     start = time.time()
                     loss = loss_fn.compute_loss(gt, preds)
+                    loss = np.expand_dims(loss, 2)
                     print(f"    Shape of loss: {loss.shape}")
-                    loss_upper[:, :, i] = loss
                     print(f"    Loss (first 4): {loss[:4, 0]}")
+                    if i==0:
+                        loss_upper = loss
+                    else:
+                        loss_upper = np.concatenate((loss_upper, loss), axis=2)
+                    
                     end = time.time()
-                    print(f"    Time taken for evaluating one datapoint: {end-start} secs.")
+                    print(f"    Time taken for evaluating one datapoint: {end-start} secs.\n")
                 # Average over all time steps
+                print(f"Shape of loss_upper: {loss_upper.shape}")
                 final_loss_upper = np.mean(loss_upper, axis=-1)
                 print(f"Final shape of loss: {final_loss_upper.shape}")
+                print(f"Final loss ATMOSPHERIC [z,q,t,u,v]: {final_loss_upper[:5, 2]}")
             end_total = time.time()
             print(f"Total time taken for evaluating all datapoints: {end_total-start_total} secs.\n")
             print(f"*** END OF EVALUATION ATMOSPHERIC VARIABLES ***\n")
